fdeeb39be5e9d69eea369dafb5424fada2c4bc28
Luka Palac <45354020+lPalac@users.noreply.github.com>
2025-05-04 22:33:45 +0200

    Restructure and changeing of redirect, auth
    Restructure and changeing of redirect, auth

--- src/app/api/signin.js
+++ src/app/api/signin.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
+  console.log("ovo je prošlo");
   res.redirect(
     "https://miro.com/oauth/authorize?response_type=code&client_id=" +
       process.env.clientID +
