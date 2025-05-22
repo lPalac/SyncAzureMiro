f9f24cb76642f75e16f5b776a410749af70ca1ef
Luka Palac <45354020+lPalac@users.noreply.github.com>
2025-05-22 19:14:43 +0200

    Visual studio installations
    Visual studio installations just default ones

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,48 +1,56 @@
-import React from 'react'; 
+import React from "react";
 
-import initMiroAPI from '../utils/initMiroAPI'; 
-import '../assets/style.css'; 
+import initMiroAPI from "../utils/initMiroAPI";
+import "../assets/style.css";
 
 const getBoards = async () => {
-    const { miro, userId } = initMiroAPI(); 
-    
-    // redirect to auth url if user has not authorized the app
-    if (!userId || !(await miro.isAuthorized(userId))) { 
-        return {
-            authUrl: miro.getAuthUrl(), 
-        }; 
-    } 
-    
-    const api = miro.as(userId); 
-    
-    const boards = []; 
-    for await (const board of api.getAllBoards()) { 
-        boards.push(board); 
-    } 
-    
+  const { miro, userId } = initMiroAPI();
+
+  // redirect to auth url if user has not authorized the app
+  if (!userId || !(await miro.isAuthorized(userId))) {
     return {
-        boards, 
-    }; 
-}; 
+      authUrl: miro.getAuthUrl(),
+    };
+  }
+
+  const api = miro.as(userId);
+
+  const boards = [];
+  for await (const board of api.getAllBoards()) {
+    boards.push(board);
+  }
+
+  return {
+    boards,
+  };
+};
 
 export default async function Page() {
-    const { boards, authUrl } = await getBoards(); 
-    
-    return ( <div> 
-      <h3>API usage demo</h3> 
-      <p className="p-small">API Calls need to be authenticated</p> 
-      <p> 
+  const { boards, authUrl } = await getBoards();
+
+  return (
+    <div>
+      <h3>API usage demo</h3>
+      <p className="p-small">API Calls need to be authenticated</p>
+      <p>
         Apps that use the API usually would run on your own domain. During
         development, test on http://localhost:3000
-      </p> 
-      {authUrl ? ( <a className="button button-primary" href={authUrl} target="_blank"> 
+      </p>
+      {authUrl ? (
+        <a className="button button-primary" href={authUrl} target="_blank">
           Login
-        </a> ) : ( <> 
-          <p>This is a list of all the boards that your user has access to:</p> 
+        </a>
+      ) : (
+        <>
+          <p>This is a list of all the boards that your user has access to:</p>
 
-          <ul> 
-            {boards?.map((board) => ( <li key={board.name}>{board.name}</li> ))} 
-          </ul> 
-        </> )} 
-    </div> ); 
-} 
+          <ul>
+            {boards?.map((board) => (
+              <li key={board.name}>{board.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
